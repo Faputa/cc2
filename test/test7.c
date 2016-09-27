9e1b1eed9aab8bea78083c72b741135dfafdca4e
kinocastle <2238932729@qq.com>
2016-09-27 08:04:25 +0800

    Add files via upload

--- test/test7.c
+++ test/test7.c
@@ -1,10 +1,7 @@
 #include "test.h"
 
 int main() {
-	int i = scanint();
-	printint(i); printstr("\n");
-	char c = scanchar();
-	printchar(c); printstr("\n");
-	char *s = scanstr();
-	printstr(s);
+	printint(scanint()); printstr("\n");
+	printchar(scanchar()); printstr("\n");
+	printstr(scanstr());
 }
