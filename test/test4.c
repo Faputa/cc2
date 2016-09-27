c495857eadb026b7ac76c85019567427ed7a2647
kinocastle <2238932729@qq.com>
2016-09-27 17:19:53 +0800

    Add files via upload

--- test/test4.c
+++ test/test4.c
@@ -3,7 +3,7 @@
 int main() {
 	for(int i = 1; i <= 9; i = i + 1) {
 		for(int j = 1; j <= i; j = j + 1) {
-			printint(j); printstr("x"); printint(i); printstr("="); printint(i * j); printstr("  ");
+			printint(j); printstr("x"); printint(i); printstr("="); printint(i * j); printstr("\t");
 		}
 		printstr("\n");
 	}
