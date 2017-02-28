61e7ae8818d0ef274107134db3a27cd710457562
kinocastle <2238932729@qq.com>
2017-02-28 16:09:06 +0800

    Add files via upload

--- test/test5.c
+++ test/test5.c
@@ -1,11 +1,12 @@
 #include "test.h"
 
 int f(int a[], int b) {
-	return a[0] + b;
+	return 0[a] + b;
 }
 int main() {
 	int a[2*5][10] = {{},{100,200,300}};
 	printint(a[1][2]);
+	//printint(2[*(1+a)]);
 	printstr("\n");
 	int *b = &a[1][1];
 	printint(*b);
