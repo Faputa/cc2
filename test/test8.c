dcf9256cc5ac46559d807d78ff23d518f5fdb38c
kinocastle <2238932729@qq.com>
2017-02-28 22:58:56 +0800

    Add files via upload

---
+++ test/test8.c
@@ -0,0 +1,18 @@
+#include "test.h"
+
+int f(int a, int b, int c, int d, int e, int f) {
+	printint(a); printstr("\n");
+	printint(b); printstr("\n");
+	printint(c); printstr("\n");
+	printint(d); printstr("\n");
+	printint(e); printstr("\n");
+	printint(f); printstr("\n");
+	int g[100] = {0, 700};
+	int h = 800;
+	printint(g[1]); printstr("\n");
+	printint(h);
+}
+
+int main() {
+	f(100, 200, 300, 400, 500, 600);
+}
